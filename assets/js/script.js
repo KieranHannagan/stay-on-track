52ac159ead9f24da276681c6f1e01d93df5089a8
Kieran Hannagan <kieranhannagan@Kierans-MacBook-Pro.local>
2021-11-15 19:55:19 -0600

    added ability to add data to local storage and display it in the text area upon refreshing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+ // moment.js setup, assigning each hour block ID tag with the correct time
 $("#currentDay").text(moment().format("dddd, MMMM Do")); 
 $("#nine").text(moment('9:00 AM', 'h:mm A').format("LT"));
 $("#ten").text(moment('10:00 AM', 'h:mm A').format("LT"));
@@ -18,76 +19,35 @@ var hourTwo = (moment('2:00 PM', 'h:mm A'));
 var hourThree = (moment('3:00 PM', 'h:mm A'));
 var hourFour = (moment('4:00 PM', 'h:mm A'));
 var hourFive = (moment('5:00 PM', 'h:mm A'));
+var saveButton = $(".saveBtn");
 
-// var eventNine = document.querySelector("#event-9am");
-// var eventTen = document.querySelector("#event-10am");
-// var eventEleven = document.querySelector("#event-11am");
-// var eventTwelve = document.querySelector("#event-12pm");
-// var eventOne = document.querySelector("#event-1pm");
-// var eventTwo = document.querySelector("#event-2pm");
-// var eventThree = document.querySelector("#event-3pm");
-// var eventFour = document.querySelector("#event-4pm");
-// var eventFive = document.querySelector("#event-5pm");
+// current time rounded down to 
+var now = moment().startOf("hour");
 
-var events = {
-  eventNine: document.querySelector("#event-9am").value,
-  eventTen: document.querySelector("#event-10am").value,
-  eventEleven: document.querySelector("#event-11am").value,
-  eventTwelve: document.querySelector("#event-12pm").value,
-  eventOne: document.querySelector("#event-1pm").value,
-  eventTwo: document.querySelector("#event-2pm").value,
-  eventThree: document.querySelector("#event-3pm").value,
-  eventFour: document.querySelector("#event-4pm").value,
-  eventFive: document.querySelector("#event-5pm").value
-}
-
-var timeInputTags = [];
-    for (let i = 0; i <= 8; i++) {
-      let y = [9,10,11,12,1,2,3,4,5]
-      let x = "pm"
-      var amChecker = function() {
-        if (i<3) {
-          x= "am";
-          
-          return x;
-        }
-      };
-      amChecker();
-      timeInputTags[i] = "#event-" + y[i] + x;  
-      y++
-    }
-
-
-
-// var timeInputTags = ["#event-9am", "#event-10am", "#event-11am", "#event-12pm", "#event-1pm", "#event-2pm", "#event-3pm", "#event-4pm", "#event-5pm"];
 var timeBgTags = ["#nine-bg", "#ten-bg", "#eleven-bg", "#twelve-bg", "#one-bg", "#two-bg", "#three-bg", "#four-bg", "#five-bg"];
 var timeArray = [hourNine, hourTen, hourEleven, hourTwelve, hourOne, hourTwo, hourThree, hourFour, hourFive];
-var populateDescriptions = function() {
-  for (let i = 0; i <= 8; i++) {
-   oldData = [
-    {
-      id: timeInputTags[i],
-      text: localStorage.getItem()
+
+var storedTasks = JSON.parse(localStorage.getItem('tasks')) || {}; 
+
+
+//loop that populates the storageTaskTags array with ID tags
+var storageTaskTags = [];
+    for (let i = 0; i <= 8; i++) {
+      let y = [9,10,11,12,1,2,3,4,5]
+      storageTaskTags[i] = "#task-" + y[i];  
     }
-  ]
-  }
-};
+    // Output of storageTaskTags: ['#task-9', '#task-10', '#task-11', '#task12', '#task1', '#task2', '#task3', '#task4', '#task5']
 
-
+  //color coordinates the hour blocks depending on the current time, uses moment.js
 var auditTime = function() {
-
   for (let i = 0; i < timeArray.length; i++) {
-    let now = moment().startOf("hour");
-    if(now>timeArray[i]) {
+    if(now>timeArray[i].startOf("hour")) {
       // add past css style
       $(timeBgTags[i]).addClass("past");
-    } 
-
-     if(now<timeArray[i]) {
+    } else if(now<timeArray[i].startOf("hour")) {
      // add future css style
       $(timeBgTags[i]).addClass("future");
-    }
-    if(now === (timeArray[i]).startOf("hour")) {
+    } else if(now = (timeArray[i]).startOf("hour")) {
      // add present css style
       $(timeBgTags[i]).addClass("present");
     }
@@ -96,39 +56,42 @@ var auditTime = function() {
 
 auditTime();
 
-var populateDescriptions = function() {
-  for (let i = 0; i <= 8; i++) {
-    if(localStorage.getItem(timeInputTags[i], inputText.value) == null) {
+// returns the task input ID tag
+function getTaskInput(buttonHour) {
 
-      var inputText = document.getElementById(timeInputTags[i]);
+  return $('#task-' +  buttonHour).val();
+}
 
-    }
-    localStorage.getItem(timeInputTags[i], inputText.value);
-    console.log(inputText.value);
-  }
+
+saveButton.each(function () {
+  var myButtonHour = $(this).data('hour');
+  var taskInput = getTaskInput(myButtonHour);
+
+  localStorage.getItem( 'tasks', JSON.stringify(taskInput));
+})
+
+// event listener that will save textarea values to local storage
+saveButton.on("click", function() {
+  console.log($(this).data('hour'));
+  var buttonHour = $(this).data('hour');
+  var taskInput = $("#task-" + buttonHour)
+  var taskValue = taskInput.val();
+
+  storedTasks[ 'task-' + buttonHour] = taskValue;
+
+  localStorage.setItem( 'tasks', JSON.stringify(storedTasks));
+});
+
+
+// function that will populate the textarea values with their corresponding values stored in local storage
+var populateInputs = function() {
+  for (let i = 0; i < 9; i++) {
+    let y = [9,10,11,12,1,2,3,4,5]
+      var myTasks = JSON.parse(localStorage.getItem('tasks'));
+      let output = $(storageTaskTags[i]);
+      output.val(myTasks["task-" + y[i]]); 
+}
 };
 
-
-// populateDescriptions();
-var saveButton = document.querySelectorAll(".save-item");
-var saveData = function () {
-  for (let i = 0; i <= 8; i++) {
-  var newData = document.getElementById(timeInputTags[i]).value;
-
-  if(localStorage.getItem("events") == null) {
-    localStorage.setItem("events", []);
-  }
-
-  var oldData = JSON.parse(localStorage.getItem("events"));
-  oldData.push(newData);
-  localStorage.setItem("events", JSON.stringify(oldData));
-
-  }
-};
-// //  save to local storage
-// saveButton.addEventListener("click", function(event){ 
-//   event.preventDefault();
-//     saveData();
-// });
-
-
+getTaskInput();
+populateInputs();
